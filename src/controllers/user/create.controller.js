f24f6109b673326946c4db77c61d2f91c0654eb2
rashidov <rashidov.aydar@mail.ru>
2021-11-20 16:31:06 +0300

    connect front & back

--- src/controllers/user/create.controller.js
+++ src/controllers/user/create.controller.js
@@ -3,9 +3,9 @@ const services = require('../../services')
 
 module.exports = socket => {
   socket.on('USER:CREATE', data => {
-    const token = md5(data.first_name + data.last_name + data.position)
-    const user = {token, data: {...data}}
-    services.user.create(user)
+    const token = md5(data.name + data.position)
+    // const user = {token, data: {...data}}
+    // services.user.create(user)
     socket.emit('USER:CREATE/TOKEN', token)
   })
 }
--- src/services/user/create.service.js
+++ src/services/user/create.service.js
@@ -2,4 +2,5 @@ const memory = require('../../memory')
 
 module.exports = user => {
   memory.users.add(user)
+  console.log('=== user memory ===', memory.users.get)
 }
